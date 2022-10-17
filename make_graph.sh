c18e76182951ec5c3edb38cfb5116485f2a783ed
Thomas Koopman <thomas.koopman@ru.nl>
2022-10-17 14:26:15 +0200

    make_graph.sh bugfix

--- make_graph.sh
+++ make_graph.sh
@@ -5,7 +5,7 @@
 
 make graph
 echo "time,pageNumber" > $1_segfaults.out
-mpirun -n $2 bin/$1_graph $3 $4 $5 2> $1_segfaults.out
+mpirun -n $2 bin/$1_graph $3 $4 $5 2>> $1_segfaults.out
 
 sed -i '/WARNING/d' $1_segfaults.out
 
