980a410583fb230be2159e1c21cf81a4674f2887
Stefan Schrijvers <ximin@ximinity.net>
2022-12-05 11:46:40 +0100

    Update cmake build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,6 @@ cmake \
 	-DGASNet_CONDUIT=mpi \
 	-DGASNet_ROOT_DIR="/usr/local/gasnet" \
 	-DSANITISE=ON \
-	-DOSHCC=/home/thomas/repos/shmemBuild/bin/oshcc \
 	-S . \
 	-B "$builddir"
 cmake --build "$builddir" -j
-
--DGASNet_ROOT_DIR="/home/ximin/tmp/gasnet/install_segmenteverything_nophsm" \
