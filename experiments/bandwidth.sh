b01615fed99d58d71ac6a1ffc931a741b7bca3b0
Thomas Koopman <tkoopman@slurm22.science.ru.nl>
2023-02-21 14:05:59 +0100

    bandwidth script formatting

--- experiments/bandwidth.sh
+++ experiments/bandwidth.sh
@@ -43,16 +43,16 @@ mkdir -p "$datadir"
 runbandwidth()
 {
     size="$1"
-    printf '%s & ', "${size}";
+    printf '%s & ' "${size}";
     "${bindir}/shray/bandwidth_normal_shray" 2 409600000 "${size}" 10;
     printf ' & ';
     mpirun.mpich -n 2 "${bindir}/mpi/bandwidth_mpi" 409600000 "${size}" 10;
-    printf ' \\\n';
+    printf ' \\\\n';
 }
 
 {
-printf '\begin{tabular}{c|c|c}\n\hline\nPacket size & Shray & MPI\n';
+printf '\\begin{tabular}{c|c|c}\n\hline\nPacket size & Shray & MPI \\\\\n';
 runbandwidth 4096;
 runbandwidth 40960000;
-printf '\end{tabular}';
+printf '\\end{tabular}';
 } > "${datadir}/bandwidth.csv" 2> "${datadir}/errors.txt"
