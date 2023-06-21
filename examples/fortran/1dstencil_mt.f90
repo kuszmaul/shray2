5a063a67e848bd91ad1dad8fd3e0532508711c93
thomas <thomas.koopman@ru.nl>
2023-06-21 16:54:47 +0200

    Multithreaded Fortran versions, perform poorly in stencil case

---
+++ examples/fortran/1dstencil_mt.f90
@@ -0,0 +1,201 @@
+#define SPACEBLOCK 10000
+#define TIMEBLOCK 100
+
+module kernels
+contains
+subroutine left(n, iterations, input, output)
+    implicit none
+
+    integer, intent(in) :: n, iterations
+    real(KIND=8) :: input(n), output(n)
+
+    integer :: t, i
+    real(KIND=8) :: temp(n), constants(3)
+
+    constants = [0.50, 0.33, 0.25]
+
+    output(1) = input(1)
+
+    do t = 1, iterations
+        do i = 2, n + iterations - 1 - t
+            output(i) = sum(input(i - 1: i + 1) * constants)
+        end do
+        if (t .ne. iterations) then
+            temp = input
+            input = output
+            output = temp
+        end if
+    end do
+end subroutine left
+
+subroutine middle(n, iterations, input, output)
+    implicit none
+
+    integer, intent(in) :: n, iterations
+    real(KIND=8) :: input(n), output(n)
+
+    integer :: t, i
+    real(KIND=8) :: temp(n), constants(3)
+
+    constants = [0.50, 0.33, 0.25]
+
+    output(1) = input(1)
+
+    do t = 1, iterations
+        do i = 2 + t, n + 2 * iterations - 1 - t
+            output(i) = sum(input(i - 1: i + 1) * constants)
+        end do
+        if (t .ne. iterations) then
+            temp = input
+            input = output
+            output = temp
+        end if
+    end do
+end subroutine middle
+
+subroutine right(n, iterations, input, output)
+    implicit none
+
+    integer, intent(in) :: n, iterations
+    real(KIND=8) :: input(n), output(n)
+
+    integer :: t, i
+    real(KIND=8) :: temp(n), constants(3)
+
+    constants = [0.50, 0.33, 0.25]
+
+    output(n + iterations) = input(n + iterations)
+
+    do t = 1, iterations
+        do i = 2 + t, n + iterations - 1
+            output(i) = sum(input(i - 1: i + 1) * constants)
+        end do
+        if (t .ne. iterations) then
+            temp = input
+            input = output
+            output = temp
+        end if
+    end do
+end subroutine right
+end module kernels
+
+module stencilModule
+contains
+subroutine BlockedStencil(n, input, output, iterations)
+    use kernels
+    implicit none
+    integer, intent(in) :: n, iterations
+    real(KIND=8), intent(inout) :: input(n)[*], output(n)[*]
+
+    real(KIND=8), allocatable :: inBuffer(:), outBuffer(:)
+    integer :: row, rowsPerImage, ending
+
+    allocate(inBuffer(SPACEBLOCK + 2 * iterations))
+    allocate(outBuffer(SPACEBLOCK + 2 * iterations))
+
+    rowsPerImage = (n / SPACEBLOCK + num_images() - 1) / num_images()
+    ending = min(rowsPerImage, n / SPACEBLOCK - (num_images() - 1) * rowsPerImage)
+
+    !$OMP PARALLEL DO
+    do row = 1, ending
+        if ((row .eq. 1) .and. (this_image() .eq. 1)) then
+            inBuffer(1:SPACEBLOCK + iterations) = input(1:SPACEBLOCK + iterations)
+            call left(SPACEBLOCK, iterations, inBuffer(:), outBuffer(:))
+            output(1:SPACEBLOCK) = outBuffer(1:SPACEBLOCK)
+        else if ((this_image() .eq. num_images()) .and. (row .eq. ending)) then
+            inBuffer(1:SPACEBLOCK + iterations) = \
+                input(1 + (row - 1) * SPACEBLOCK - iterations:row * SPACEBLOCK)
+            call right(SPACEBLOCK, iterations, inBuffer(:), outBuffer(:))
+            output((row - 1) * SPACEBLOCK + 1:row * SPACEBLOCK) = outBuffer(1:SPACEBLOCK)
+        else
+            if (row .eq. 1) then
+                inBuffer(1:iterations) = &
+                    input(rowsPerImage * SPACEBLOCK - iterations + 1: rowsPerImage * SPACEBLOCK)[this_image() - 1]
+                inBuffer(iterations + 1:SPACEBLOCK + 2 * iterations) = &
+                    input(1:SPACEBLOCK + iterations)
+            else if (row .eq. ending) then
+                inBuffer(1:SPACEBLOCK + iterations) = &
+                    input((row - 1) * SPACEBLOCK - iterations + 1:row * SPACEBLOCK)
+                inBuffer(SPACEBLOCK + iterations + 1:SPACEBLOCK + 2 * iterations) = \
+                    input(1: iterations)[this_image() + 1]
+            else
+                inBuffer(1:SPACEBLOCK + 2 * iterations) = &
+                input(1 + (row - 1) * SPACEBLOCK - iterations : row * SPACEBLOCK + iterations)
+            end if
+            call middle(SPACEBLOCK, iterations, inBuffer(:), outBuffer(:))
+            output((row - 1) * SPACEBLOCK + 1:row * SPACEBLOCK) = &
+                   outBuffer(iterations + 1:iterations + SPACEBLOCK)
+        end if
+    end do
+    !$OMP END PARALLEL DO
+
+    sync all
+
+    deallocate(inBuffer)
+    deallocate(outBuffer)
+end subroutine BlockedStencil
+
+subroutine Stencil(n, input, output, iterations)
+    implicit none
+    integer, intent(in) :: n, iterations
+    real(KIND=8), intent(inout) :: input(n)[*], output(n)[*]
+
+    integer :: t
+
+    do t = TIMEBLOCK, iterations, 2 * TIMEBLOCK
+        call BlockedStencil(n, input, output, TIMEBLOCK)
+        call BlockedStencil(n, output, input, TIMEBLOCK)
+    end do
+    if (mod(iterations, TIMEBLOCK) .ne. 0) then
+        call BlockedStencil(n, input, output, mod(iterations, TIMEBLOCK))
+    end if
+end subroutine Stencil
+end module stencilModule
+
+program main
+    use stencilModule
+    implicit none
+
+    integer :: n, iterations
+    integer :: cpu_count, cpu_count2, count_rate, count_max
+    real(KIND=8), allocatable :: input(:)[:], output(:)[:]
+    character(len=12), dimension(:), allocatable :: args
+
+    allocate(args(2))
+    call get_command_argument(1, args(1))
+    read (unit=args(1), fmt=*) n
+
+    call get_command_argument(2, args(2))
+    read (unit=args(2), fmt=*) iterations
+
+    if (modulo(n, SPACEBLOCK) /= 0) then
+        write (*, *) 'Please make sure n is divisible by ', SPACEBLOCK
+    end if
+
+    if (modulo(iterations, 2) /= 0) then
+        write (*, *) 'Please make sure iterations / TIMEBLOCK is even. Suggestion: ', &
+             iterations / TIMEBLOCK / 2 * 2
+    end if
+
+
+    allocate(input(n / num_images())[*])
+    allocate(output(n / num_images())[*])
+
+    input = 1
+    output = 1
+
+    sync all
+    call system_clock(cpu_count, count_rate, count_max)
+
+    call Stencil(n, input, output, iterations)
+    sync all
+    call system_clock(cpu_count2, count_rate, count_max)
+
+    if (this_image() .eq. 1) then
+        write (*, *) 5.0 * (n - 2) * iterations / 1000000000.0 &
+            / (real(cpu_count2 - cpu_count) / count_rate)
+    end if
+
+    deallocate(input)
+    deallocate(output)
+end program main
---
+++ examples/fortran/monopoly_mt.f90
@@ -0,0 +1,127 @@
+! https://people.eecs.berkeley.edu/~aydin/csb2009.pdf
+
+module CSR_SPMV
+    implicit none
+
+    type, public :: CSRMatrix
+        ! The matrix is m x n
+        integer :: m
+        integer :: n
+        ! Stores the non-zeroes
+        real(KIND=8), allocatable :: val(:)
+        ! The first non-zero of row k is the row_ptr(k)th non-zero overall.
+        ! row_ptr(m + 1) is the total number of non-zeroes.
+        integer, allocatable :: row_ptr(:)
+        ! The kth non-zero has column-index col_ind(k)
+        integer, allocatable :: col_ind(:)
+    end type
+
+contains
+    ! Computes the local part of matmul(mat, output), where mat
+    ! is the local part of a sparse matrix distributed blockwise along
+    ! the first dimension.
+    function spmv(mat, input) result(output)
+        implicit none
+        type(CSRMatrix), intent(in) :: mat
+        real(KIND=8), intent(in) :: input((mat%n + num_images() - 1) / num_images())[*]
+        real(KIND=8), allocatable :: output(:)
+
+        integer :: row, col, j, blockSize, processor, localIndex
+
+        blockSize = (mat%n + num_images() - 1) / num_images()
+        allocate(output(mat%m))
+
+        !$OMP PARALLEL DO
+        do row = 1, mat%m
+            output(row) = 0.0
+            do j = mat%row_ptr(row), mat%row_ptr(row + 1) - 1
+                col = mat%col_ind(j)
+                localIndex = modulo(col - 1, blockSize) + 1
+                processor = (col - 1) / blockSize + 1
+                output(row) = output(row) + mat%val(j) * input(localIndex)[processor]
+            end do
+        end do
+        !$OMP END PARALLEL DO
+    end function spmv
+
+    ! Computes the local part of the steady state vector of a distributed stochastic matrix.
+    ! mat is the local part of this matrix, distributed blockwise along the first dimension.
+    function steady_state(mat, iterations) result(state)
+        implicit none
+        type(CSRMatrix), intent(in) :: mat
+        integer, intent(in) :: iterations
+        real(KIND=8), allocatable :: state(:), input(:)[:]
+        integer :: t
+
+        allocate(input(mat%m)[*])
+        allocate(state(mat%m))
+
+        state = 1 / mat%n
+
+        do t = 1,iterations
+            input = spmv(mat, input)
+            sync all
+        end do
+
+        state = input
+
+        deallocate(input)
+    end function
+
+end module CSR_SPMV
+
+
+program main
+    use CSR_SPMV
+    implicit none
+
+    integer :: iterations
+    ! Timing
+    integer :: cpu_count, cpu_count2, count_rate, count_max
+    integer(KIND=8) :: totalNz
+    integer :: n, global_row, blockSize, row, eyes, i
+    character(len=12) :: iterationArg, nArg
+    type(CSRMatrix) :: mat
+    real(KIND=8), allocatable :: res(:)
+    real(KIND=8) :: probabilities(12)
+
+    ! Parse the distributed array
+    call get_command_argument(1, nArg)
+    call get_command_argument(2, iterationArg)
+    read (nArg, *) n
+    read (iterationArg, *) iterations
+
+    blockSize = (n + num_images() - 1) / num_images()
+    mat%m = merge(n - (num_images() - 1) * blockSize, blockSize, this_image() .eq. num_images())
+    mat%n = n
+    allocate(mat%val(11 * mat%m))
+    allocate(mat%row_ptr(mat%m + 1))
+    allocate(mat%col_ind(11 * mat%m))
+    allocate(res(mat%n))
+
+    probabilities = [0.0, 1.0 / 36, 2.0 / 36, 3.0 / 36, 4.0 / 36, 5.0 / 36, 6.0 / 36, &
+                     5.0 / 36, 4.0 / 36, 3.0 / 36, 2.0 / 36, 1.0 / 36]
+    i = 0
+    do row = 1, mat%m
+        global_row = (this_image() - 1) * blockSize + row
+        do eyes = 2, 12
+            mat%val(i) = probabilities(eyes)
+            mat%col_ind(i) = modulo(global_row + eyes, n) + 1
+            i = i + 1
+        end do
+        mat%row_ptr(row) = 11 * (row - 1) + 1
+    end do
+    mat%row_ptr(mat%m + 1) = 11 * mat%m + 1
+
+    call system_clock(cpu_count, count_rate, count_max)
+
+    res = steady_state(mat, iterations)
+
+    call system_clock(cpu_count2, count_rate, count_max)
+
+    totalNz = 11 * n
+    if (this_image() .eq. 1) then
+        write (*, *) 2.0 * real(totalNz) * iterations / 1000000000.0 / &
+            (real(cpu_count2 - cpu_count) / count_rate)
+    end if
+end program main
